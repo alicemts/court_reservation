9d7d73bef08c1733433869ca64b7e85e51adbb86
Aggelos Troulakis <atroulakis@softone.gr>
2024-10-13 02:19:35 +0300

    UserController

--- court_reservation/app/Http/Controllers/UserController.php
+++ court_reservation/app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    // Get all users (READ)
+    // Get all users (READ) ψηεψκ
     public function index()
     {
         $users = User::all();
