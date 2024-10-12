a88b0ee14ff2db465ab482bcbecc6599fd6411dc
Aggelos Troulakis <atroulakis@softone.gr>
2024-10-13 02:07:03 +0300

    UserController

---
+++ court_reservation/app/Http/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    // Get all users (READ)
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    // Get a specific user (READ)
+    public function show($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            return response()->json($user);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+
+    // Create a new user (CREATE)
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json($user, 201);  // 201 Created
+    }
+
+    // Update an existing user (UPDATE)
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if ($user) {
+            $user->update($request->all());
+            return response()->json($user);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+
+    // Delete a user (DELETE)
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if ($user) {
+            $user->delete();
+            return response()->json(['message' => 'User deleted']);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+}
